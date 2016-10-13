9aaaa9b209243210c3885ce0e8ec96871069061f
ken aoki <aokiken@me.com>
2016-10-13 10:59:13 +0900

    .eslintrc.jsonを調整

--- src/lib/directoriesWrite.js
+++ src/lib/directoriesWrite.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 
-const isDir = ({ filePath }) => fs.existsSync(filePath) && fs.statSync(filePath).isDirectory();
+const isDir = (filePath) => fs.existsSync(filePath) && fs.statSync(filePath).isDirectory();
 
 export default (styleguideDirName, sassDirPath) =>
   new Promise((resolve) => {
--- src/lib/spaCopy.js
+++ src/lib/spaCopy.js
@@ -1,4 +1,4 @@
 import fse from 'fs-extra';
 
-export default ({ styleguideDirName }) =>
-  new Promise(({ resolve }) => fse.copySync(`${__dirname}/../spa`, styleguideDirName, null, () => resolve()));
+export default (styleguideDirName) =>
+  new Promise((resolve) => fse.copySync(`${__dirname}/../spa`, styleguideDirName, null, () => resolve()));
